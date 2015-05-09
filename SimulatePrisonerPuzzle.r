f4f3b110fcb53afc7d2466847a762d588c77faa0
unknown <davidp6@IHMELT071.ihme.washington.edu>
2015-05-08 17:57:19 -0700

    Added readme

--- SimulatePrisonerPuzzle.r
+++ SimulatePrisonerPuzzle.r
@@ -13,16 +13,6 @@
 # Implementation of a new sampling strategy requires a function to define it. Pass the name of the function to SimulateSampling.r and it will run
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Prompt: 
-#
-# The names of 100 prisoners are placed in 100 wooden boxes, one name to a box, and the boxes are lined up on a table in a room.
-# One by one, the prisoners are led into the room; each may look in at most 50 boxes, but must leave the room exactly as he found it and is permitted no further communication with the others.
-# The prisoners have a chance to plot their strategy in advance, and they are going to need it, because unless every single prisoner finds his own name all will subsequently be executed.
-# Find a strategy for them which has probability of success exceeding 30%.
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 # -------------------------------------------------------------------------------------------------------------------------
 # Set up R
 rm(list=ls())
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Code that runs simulations for the prisoner puzzle that Roy shared with us.
+-----
+
+# Prompt: 
+
+## The names of 100 prisoners are placed in 100 wooden boxes, one name to a box, and the boxes are lined up on a table in a room.
+## One by one, the prisoners are led into the room; each may look in at most 50 boxes, but must leave the room exactly as he found it and is permitted no further communication with the others.
+## The prisoners have a chance to plot their strategy in advance, and they are going to need it, because unless every single prisoner finds his own name all will subsequently be executed.
+## Find a strategy for them which has probability of success exceeding 30%.
+-----
+
+# Code:
+## SimulatePrisonerPuzzle.r 	: main code that runs everything else
+## SimulateSampling.r			: function that executes multiple iterations of a given sampling strategy
+## Random.r						: function that implements a completely random sampling strategy
+-----
